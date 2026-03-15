b9e6e949ece64496806e83479d9ca067bb3420af
Abhishek-Singh2016 <abhisheksingh050593@gmail.com>
2026-03-15 18:10:34 +0530

    corrected mysql docker file name

--- database-sql/build-image-kubernetes.sh
+++ database-sql/build-image-kubernetes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build image using the dockerfile present in the folder.
-docker build -f dockerfile -t image_mov_app_mysql .
+docker build -f dockerfile-model -t image_mov_app_mysql .
 
 # Load the image to the Minikube cluster.
 #minikube image load image_mov_app_mysql:latest
