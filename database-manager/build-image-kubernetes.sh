62f8f650f54616471988ccd048381aa28aebfbfb
Abhishek-Singh2016 <abhisheksingh050593@gmail.com>
2026-03-16 13:20:29 +0530

    corrected build command

--- database-manager/build-image-kubernetes.sh
+++ database-manager/build-image-kubernetes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build image using the dockerfile present in the folder.
-docker build -f dockerfile -t image_mov_app_python .
-docker build -t $ECR_REGISTRY/$REPO:Python-mysql-app-$TAG dockerfile
+#docker build -f dockerfile -t image_mov_app_python .
+docker build -f dockerfile -t $ECR_REGISTRY/$REPO:Python-mysql-app-$TAG .
 docker push $ECR_REGISTRY/$REPO:Python-mysql-app-$TAG
-ERROR: failed to build: invalid tag "702175642104.dkr.ecr.us-east-1.amazonaws.com/:mysql-db-": invalid reference format
+#ERROR: failed to build: invalid tag "702175642104.dkr.ecr.us-east-1.amazonaws.com/:mysql-db-": invalid reference format
 
 # Load the image to the Minikube cluster.
 #minikube image load image_mov_app_python:latest
--- database-sql/build-image-kubernetes.sh
+++ database-sql/build-image-kubernetes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Build image using the dockerfile present in the folder.
-docker build -f dockerfile-model -t image_mov_app_mysql .
-docker build -t $ECR_REGISTRY/$REPO:mysql-db-$TAG dockerfile-model
+#docker build -f dockerfile-model -t image_mov_app_mysql .
+docker build -f dockerfile-model -t $ECR_REGISTRY/$REPO:mysql-db-$TAG .
 docker push $ECR_REGISTRY/$REPO:mysql-db-$TAG
 
 # Load the image to the Minikube cluster.
