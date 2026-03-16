255943043ce9c50ff86d3b63fd965f73f290ce38
Abhishek-Singh2016 <abhisheksingh050593@gmail.com>
2026-03-16 12:50:11 +0530

    build push

--- database-manager/build-image-kubernetes.sh
+++ database-manager/build-image-kubernetes.sh
@@ -2,6 +2,8 @@
 
 # Build image using the dockerfile present in the folder.
 docker build -f dockerfile -t image_mov_app_python .
+docker build -t $ECR_REGISTRY/$REPO:Python-mysql-app-$TAG dockerfile
+docker push $ECR_REGISTRY/$REPO:Python-mysql-app-$TAG
 
 # Load the image to the Minikube cluster.
 #minikube image load image_mov_app_python:latest
--- database-sql/build-image-kubernetes.sh
+++ database-sql/build-image-kubernetes.sh
@@ -2,6 +2,8 @@
 
 # Build image using the dockerfile present in the folder.
 docker build -f dockerfile-model -t image_mov_app_mysql .
+docker build -t $ECR_REGISTRY/$REPO:mysql-db-$TAG dockerfile-model
+docker push $ECR_REGISTRY/$REPO:mysql-db-$TAG
 
 # Load the image to the Minikube cluster.
 #minikube image load image_mov_app_mysql:latest
