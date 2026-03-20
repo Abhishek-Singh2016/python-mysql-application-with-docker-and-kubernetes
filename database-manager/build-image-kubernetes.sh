71dd82a919a05b4dd767b1336afbb940a0ff4be3
Abhishek-Singh2016 <abhisheksingh050593@gmail.com>
2026-03-20 11:32:17 +0530

    build and test

--- database-manager/build-image-kubernetes.sh
+++ database-manager/build-image-kubernetes.sh
@@ -6,6 +6,10 @@ docker build -f dockerfile -t $ECR_REGISTRY/$REPO:Python-mysql-app-$TAG .
 docker push $ECR_REGISTRY/$REPO:Python-mysql-app-$TAG
 #ERROR: failed to build: invalid tag "702175642104.dkr.ecr.us-east-1.amazonaws.com/:mysql-db-": invalid reference format
 
+#Deployment
+kubectl apply -f ../python-app-deployment.yaml
+kubectl apply -f ../python-app-service.yaml
+
 # Load the image to the Minikube cluster.
 #minikube image load image_mov_app_python:latest
-docker image list
+#docker image list
