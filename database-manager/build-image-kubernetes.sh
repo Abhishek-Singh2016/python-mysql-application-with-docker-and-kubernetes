e20dd52c9c8846b3baeda6390f9bd7411c007262
Abhishek-Singh2016 <abhisheksingh050593@gmail.com>
2026-03-20 23:36:59 +0530

    intergrate newrelic

--- database-manager/build-image-kubernetes.sh
+++ database-manager/build-image-kubernetes.sh
@@ -2,6 +2,7 @@
 
 # Build image using the dockerfile present in the folder.
 #docker build -f dockerfile -t image_mov_app_python .
+
 docker build -f dockerfile -t $ECR_REGISTRY/$REPO:Python-mysql-app-$TAG .
 docker push $ECR_REGISTRY/$REPO:Python-mysql-app-$TAG
 #ERROR: failed to build: invalid tag "702175642104.dkr.ecr.us-east-1.amazonaws.com/:mysql-db-": invalid reference format
